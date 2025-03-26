91c65c4ba2c18dcc82672d58568a9f7061fb8e09
ike-nanino <ikesemanu@gmail.com>
2025-03-26 08:03:43 -0400

    adding 2001 to dashboard after i removed it

--- components/profile/DashboardSummary.tsx
+++ components/profile/DashboardSummary.tsx
@@ -4,7 +4,7 @@ const DashboardSummary = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       <div className="bg-white p-6 rounded-md shadow-md">
-        <h2 className="text-lg font-semibold">Item Deposit Value </h2>
+        <h2 className="text-lg font-semibold">Item Deposit Value <span className='text-base font-medium text-gray-600'>(2001)</span> </h2>
         <p className="text-xl text-gray-600 font-medium">$4,536,477.54</p>
       </div>
       <div className="bg-white p-6 rounded-md shadow-md">
